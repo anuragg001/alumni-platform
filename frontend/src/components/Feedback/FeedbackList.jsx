bf522407bac0b063ce8f294f36c42c18c24a1790
anurag jaiswal <anuraggjaiswal@gmail.com>
2024-09-01 21:28:52 +0530

    Resolved issue

--- frontend/src/components/Feedback/FeedbackList.jsx
+++ frontend/src/components/Feedback/FeedbackList.jsx
@@ -18,6 +18,8 @@ const FeedbackList = () => {
     fetchFeedbacks();
   };
 
+
+
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-2xl font-bold mb-4">Feedback</h1>
